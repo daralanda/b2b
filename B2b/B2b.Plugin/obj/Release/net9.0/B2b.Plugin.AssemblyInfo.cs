ea06d66ab2d34397c652f73973858f5882710707
Hıdır İşitmez <hidirisitmez@gmail.com>
2025-12-19 11:51:34 +0300

    bug fix

--- B2b/B2b.Plugin/obj/Release/net9.0/B2b.Plugin.AssemblyInfo.cs
+++ B2b/B2b.Plugin/obj/Release/net9.0/B2b.Plugin.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("B2b.Plugin")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+415e27ddf1b98c52645ab0643c78e7c1fd957f35")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+16b5c6c8cefaf9567a98f54e20b92a73dc028af1")]
 [assembly: System.Reflection.AssemblyProductAttribute("B2b.Plugin")]
 [assembly: System.Reflection.AssemblyTitleAttribute("B2b.Plugin")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
